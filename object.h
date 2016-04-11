2a5ba10b881c0d560f49c3276cd491223f7e1a05
Daria Sova <todariasova@gmail.com>
2016-04-10 17:37:10 -0700

    WIP: small changes
    Change-Id: I35d67e11da3c04fec98df8137e07303656cbd5ca

--- object.h
+++ object.h
@@ -9,8 +9,9 @@ public:
     vec3 Center;
     float Radius;
     vec3 ObjectColor;
+    bool Reflect;
 
-    Object(vec3 Center_in=vec3(0,0,0), float Radius_in=0, vec3 ObjectColor_in=vec3(0,0,0)) : Center(Center_in), Radius(Radius_in) , ObjectColor(ObjectColor_in){}
+    Object(vec3 Center_in=vec3(0,0,0), float Radius_in=0, vec3 ObjectColor_in=vec3(0,0,0), bool reflect=false) : Center(Center_in), Radius(Radius_in) , ObjectColor(ObjectColor_in), Reflect(reflect){}
     //camera was origin
     virtual bool Intersect(vec3 Camera, vec3 Direction, float  *t, vec3 * normal, vec3* tpix) = 0;
 
--- raytrace.h
+++ raytrace.h
@@ -14,8 +14,8 @@ using namespace glm;
 std::vector<Object *> pObjectList;
 std::vector<Light *> pLightList;
 
-float TMin = 99999999;
-vec3 NormMin;
+float t_min = 99999999;
+vec3 Norm_min;
 vec3 PixelColour;
 Pixel px;
 vec3 DirArray[4];
@@ -23,6 +23,34 @@ vec3 PixelArray[4];
 vec3 Camera(256, 256, -400);
 vec3 DiffuseColour(255, 128, 128);
 
+/*inline vec3 CalcReflection(vec3 IP, vec3 r)
+{
+    bool IntersectCheck = false;
+    vec3 Colour;
+    for (int i = 0; i < pObjectList.size(); ++i)
+    {
+        float t;
+        vec3 Normal;
+        vec3 Temp;
+        bool DoesIntersect;
+        //if (pObjectList[i].Object == 0) DoesIntersect = pObjectList[i].IsSphereIntersect(IP, r, &t, &Normal, &Temp);
+        DoesIntersect = pObjectList[i]->Intersect(IP, r, &t, &Normal, &Temp);
+
+
+        if (DoesIntersect)
+        {
+            IntersectCheck = true;
+            if (t_min > t)
+            {
+                Colour = Temp;
+                t_min = t;
+            }
+        }
+    }
+
+    if (!IntersectCheck) return vec3(0, 0, 0) - DiffuseColour;
+    return Colour;
+}*/
 
 inline void SetColour(Pixel& px, vec3 CalculatedColor)
 {
@@ -41,7 +69,7 @@ inline void SetColour(Pixel& px, vec3 CalculatedColor)
     px.A = 255;
 }
 
-inline void InitShapes()
+inline void InitializeObjects()
 {
 
 
@@ -79,10 +107,7 @@ inline void InitShapes()
     pObjectList.push_back(&Sphere1);
     pObjectList.push_back(&Sphere2);
 
-
-    //struct LightStruct
     Light Light1(vec3(256, 256, -200), vec3(0,0,0), vec3(0,0,0), 0.0, false);
-    //struct LightStruct
     Light Light2(vec3(300, 200, -150), vec3(0,0,0), vec3(0,0,0), 0.0, false);
 
     pLightList.push_back(&Light1);
@@ -91,30 +116,30 @@ inline void InitShapes()
 
 }
 
-inline void InitAA(float xpos, float ypos, vec3 DirArray[4])
+inline void InitializeAntiAliasing(float xp, float yp, vec3 DirArray[4])
 {
-    vec3 PixelPos(ypos - 0.25f, xpos - 0.25f, 0);
-    vec3 Direction = PixelPos - Camera;
+    vec3 PxPos(yp - 0.25f, xp - 0.25f, 0);
+    vec3 Direction = PxPos - Camera;
     Direction = normalize(Direction);
     DirArray[0] = Direction;
 
-    PixelPos = vec3(ypos - 0.25f, xpos + 0.25f, 0);
-    Direction = PixelPos - Camera;
+    PxPos = vec3(yp - 0.25f, xp + 0.25f, 0);
+    Direction = PxPos - Camera;
     Direction = normalize(Direction);
     DirArray[1] = Direction;
 
-    PixelPos = vec3(ypos + 0.25f, xpos + 0.25f, 0);
-    Direction = PixelPos - Camera;
+    PxPos = vec3(yp + 0.25f, xp + 0.25f, 0);
+    Direction = PxPos - Camera;
     Direction = normalize(Direction);
     DirArray[2] = Direction;
 
-    PixelPos = vec3(ypos + 0.25f, xpos - 0.25f, 0);
-    Direction = PixelPos - Camera;
+    PxPos = vec3(yp + 0.25f, xp - 0.25f, 0);
+    Direction = PxPos - Camera;
     Direction = normalize(Direction);
     DirArray[3] = Direction;
 }
 
-inline bool CalcClosestIntersection(vec3 Direction)
+inline bool ClosestIntersection(vec3 Direction)
 {
     bool IntersectCheck = false;
     for (int i = 0; i < pObjectList.size(); ++i)
@@ -130,21 +155,28 @@ inline bool CalcClosestIntersection(vec3 Direction)
         if (DoesIntersect)
         {
             IntersectCheck = true;
-            if (TMin > t)
+            if (t_min > t)
             {
+                /*if (pObjectList[i].reflect == true)
+                {
+                    vec3 IntersectionPoint = pObjectList[i].ip;
+                    vec3 r = Direction - 2 * dot(Direction, ObjectList[i].Normal) * ObjectList[i].Normal;
+                        Temp  = CalcReflection(IntersectionPoint, r);
+                 }*/
+
                 PixelColour = Temp;
-                TMin = t;
-                NormMin = Normal;
+                t_min = t;
+                Norm_min = Normal;
             }
         }
     }
     return IntersectCheck;
 }
 
-inline int CalcLight(int NumShadows, vec3 Direction)
+inline int CalculateLight(int NumShadows, vec3 Direction)
 {
 
-    vec3 Intersection = Direction * TMin;
+    vec3 Intersection = Direction * t_min;
     Intersection = Intersection + Camera;
     for (int k = 0; k < pLightList.size(); k++)
     {
@@ -180,18 +212,17 @@ inline int CalcLight(int NumShadows, vec3 Direction)
     return NumShadows;
 }
 
-inline void CalcColour(int NumShadows)
+inline void CalculateColor(int NumShadows)
 {
     float DiffuseTerm = 0;
     int i;
     for (i = 0; i < pLightList.size(); i++)
     {
-        DiffuseTerm += dot(pLightList[i]->LightVector, NormMin);
+        DiffuseTerm += dot(pLightList[i]->LightVector, Norm_min);
     }
     DiffuseTerm = DiffuseTerm / i;
     if (DiffuseTerm > 0)
     {
-        //*0.5
         vec3 PixelDiffuseColour = DiffuseColour * DiffuseTerm;
         PixelColour = PixelColour + PixelDiffuseColour;
     }
@@ -199,13 +230,13 @@ inline void CalcColour(int NumShadows)
     for (i = 0; i < NumShadows; i++)
     {
         //Shadow Intensity
-        PixelColour = PixelColour * 0.4f;
+        PixelColour = PixelColour * 0.3f;
     }
 }
 
 inline void RayTrace(Image* pImage)
 {
-    InitShapes();
+    InitializeObjects();
 
     for (int i = 0; i < 512; ++i)
         for (int j = 0; j < 512; ++j)
@@ -214,18 +245,18 @@ inline void RayTrace(Image* pImage)
             float ypos = j;
             bool NotBackground = false;
 
-            InitAA(xpos, ypos, DirArray);
+            InitializeAntiAliasing(xpos, ypos, DirArray);
 
             //anti-ailiasing
             for (int k = 0; k < 4; k++){
-                TMin = 999999;
+                t_min = 999999;
                 //check if and what ray is intersecting
-                if (CalcClosestIntersection(DirArray[k]))
+                if (ClosestIntersection(DirArray[k]))
                 {
-                    int NumShadows = 0;
-                    NumShadows = CalcLight(NumShadows, DirArray[k]);
+                    int ShadowsNum = 0;
+                    ShadowsNum = CalculateLight(ShadowsNum, DirArray[k]);
 
-                    CalcColour(NumShadows);
+                    CalculateColor(ShadowsNum);
                     PixelArray[k] = PixelColour;
                     NotBackground = true;
                 }
